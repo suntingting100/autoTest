97e90601ce2699bd974985cb8674508877b29b42
suntingting100 <617587058@qq.com>
2020-07-14 21:46:38 +0800

    增加log文件

--- common/Log.py
+++ common/Log.py
@@ -1,1 +1,17 @@
 # 调用该类的方法，用来打印生成日志
+
+import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from testfile import getpathInfo
+
+path = getpathInfo.get_Path()
+log_path = os.path.join(path, "result") # 存放log文件的路径
+
+
+class Logger(object):
+    def __init__(self, logger_name="log..."):
+        self.logger = logging.getLogger(logger_name)
+        logging.root.setLevel(logging.NOTSET)
+        self.log_file_name = "Logs"  # 日志文件的名称
+        
