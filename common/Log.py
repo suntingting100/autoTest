d9ca9ff46e77cebc13736ea9623afa42ad1979aa
zhaojie <617687058@qq.com>
2020-07-15 19:49:51 +0800

    补充log文件

--- common/Log.py
+++ common/Log.py
@@ -14,9 +14,34 @@ class Logger(object):
         self.logger = logging.getLogger(logger_name)
         logging.root.setLevel(logging.NOTSET)
         self.log_file_name = "Logs"  # 日志文件的名称
+        self.backup_count = 5 # 最多存放日志的数量
+        # 日志输出级别
+        self.console_output_level = "WARNING"
+        self.file_out_level = "DEBUG"
+        # 日志输出格式
+        self.formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-        ###111
-        
-        
-       # 222远程分支上改的
-        
+    def get_logger(self)
+        """
+        在logger中添加日志句柄并返回，如果logger中已有句柄，则直接返回
+        :return: 
+        """
+        if not self.logger.handlers: # 避免重复日志
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(self.formatter)
+            console_handler.setLevel(self.console_output_level)
+            self.logger.handlers(console_handler)
+
+            # 每天重新创建一个日志文件，最多保留back_up份
+            file_handler = TimedRotatingFileHandler(filename=os.path.join(log_path, self.log_file_name), when="D,",
+                                                    interval=1, backupCount=self.backup_count, delay=True,
+                                                    encoding="utf-8")
+            file_handler.setFormatter(self.formatter)
+            file_handler.setLevel(self.file_out_level)
+            self.logger.addHandler(file_handler)
+        return self.logger
+
+logger = Logger().get_logger()
+
+
+
